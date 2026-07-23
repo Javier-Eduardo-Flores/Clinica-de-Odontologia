ca953ee24c8bd4770d25fc05560b536deac5bdb9
Jonathan Ordoñez <ordonezjonathan52@gmail.com>
2026-07-22 18:31:26 -0600

    Agregando odontograma con estilos y con config

---
+++ app/components/odontograma/Odontograma.tsx
@@ -0,0 +1,15 @@
+// Odontograma.tsx
+'use client';
+
+import type { DienteConEstado } from './odontograma.types';
+import { Diente } from './Diente';
+
+export function Odontograma({ dientes }: { dientes: DienteConEstado[] }) {
+  return (
+    <svg viewBox="0 0 800 260" className="w-full h-auto">
+      {dientes.map((diente) => (
+        <Diente key={diente.id_diente} diente={diente} />
+      ))}
+    </svg>
+  );
+}
